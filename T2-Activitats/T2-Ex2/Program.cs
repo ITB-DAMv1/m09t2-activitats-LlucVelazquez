469fb42aead196e0297fe2a7d38d8668207bd294
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-24 12:52:42 +0200

    a

--- T2-Activitats/T2-Ex2/Program.cs
+++ T2-Activitats/T2-Ex2/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace T2_Ex2
+﻿using System.Diagnostics;
+
+namespace T2_Ex2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+			//Executa el programa dotnet amb l’argument –info. 
+			var process = new Process
+			{
+				//Configurem el process amb la classe ProcessStartInfo
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "dotnet",
+					Arguments = "--info",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				}
+			};
+
+			process.Start();
+
+			//Capturem el que s’ha imprés per pantalla:
+			string output = process.StandardOutput.ReadToEnd();
+			process.WaitForExit();
+			Console.WriteLine(output);
+			var processos = Process.GetProcesses();
+			foreach (Process os in processos)
+			{
+				Console.WriteLine($"PID: {os.Id} \t Name: {os.ProcessName} ");
+			}
+			int pid = int.Parse(Console.ReadLine());
+		}
+	}
 }
