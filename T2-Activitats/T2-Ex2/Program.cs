c8ba23002563f73bec70e7bf789a51ecf61a2c14
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-24 13:59:41 +0200

    c

--- T2-Activitats/T2-Ex2/Program.cs
+++ T2-Activitats/T2-Ex2/Program.cs
@@ -4,7 +4,10 @@ namespace T2_Ex2
 {
     internal class Program
     {
-        static void Main(string[] args)
+		public static bool isPing = true;
+		public static int Rounds = 10;
+		public static object Locker = new object();
+		static void Main(string[] args)
         {/*
 			//Executa el programa dotnet amb l’argument –info. 
 			var process = new Process
@@ -33,14 +36,58 @@ namespace T2_Ex2
 			}
 			int pid = int.Parse(Console.ReadLine());*/
 
-			Thread th = new Thread(Compte);
-			th.Start();
+			Thread th1 = new Thread(Ping);
+			Thread th2 = new Thread(Pong);
+			th1.Start();
+			th2.Start();
+
+			th1.Join();
+			th2.Join();
 		}
-		static void Compte()
+		static void ReverseCount()
 		{
 			for (int i = 10; i > 0; i--)
 			{
-				Console.WriteLine(i);
+				Console.WriteLine("RCount: "+ i);
+				Thread.Sleep(3000);
+			}
+		}
+		static void Count()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine("Count "+ i);
+				Thread.Sleep(1000);
+			}
+		}
+		
+		static void Ping()
+		{
+			
+			for (int i = 0; i < Rounds; i++)
+			{
+				lock (Locker)
+				{
+					while (!isPing)
+					{
+						Monitor.Wait(Locker);
+					}
+					Console.WriteLine("Ping");
+					isPing = false;
+				}
+				
+			}
+		}
+		static void Pong()
+		{
+			for (int i = 0; i < Rounds; i++)
+			{
+				lock (Locker)
+				{
+					Console.WriteLine("\t Pong");
+					isPing = true;
+				}
+				
 			}
 		}
 	}
